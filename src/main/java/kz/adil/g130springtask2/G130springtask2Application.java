8a011bfd29476a5ce171f994d11d98974d609ab3
ASUS <adilhanmoldamurat10@gmail.com>
2024-11-29 17:08:13 +0500

    initial commit

---
+++ src/main/java/kz/adil/g130springtask2/G130springtask2Application.java
@@ -0,0 +1,13 @@
+package kz.adil.g130springtask2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G130springtask2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(G130springtask2Application.class, args);
+    }
+
+}
---
+++ src/test/java/kz/adil/g130springtask2/G130springtask2ApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.adil.g130springtask2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class G130springtask2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
